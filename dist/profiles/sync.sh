210804371a652ebcd77350d876142d13922e837f
Justin Hall <justin.hall@roundtriphealth.com>
2020-12-16 10:01:02 -0500

    initial commit

--- dist/profiles/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sync() {
-  rsync -av  --exclude $ROUNDTRIP_WORKSPACE_DIRECTORY/databases --exclude $ROUNDTRIP_WORKSPACE_DIRECTORY/mobile  --exclude $ROUNDTRIP_WORKSPACE_DIRECTORY/archived — progress -e "ssh -i /Users/sam.birk/.ssh/aws-cali.pem" $ROUNDTRIP_WORKSPACE_DIRECTORY/ ubuntu@ec2-54-183-121-206.us-west-1.compute.amazonaws.com:/home/ubuntu/workspace
-}
-
-ssh-dev() {
-  ssh -i $HOME/.ssh/aws-cali.pem ubuntu@ec2-54-183-121-206.us-west-1.compute.amazonaws.com
-}
